89291bc86e14602bd9270818772b37afd37801d0
Cameron Leger <contact@cameronleger.com>
2022-03-31 17:27:07 -0600

    Release & Ignore

--- release.sh
+++ release.sh
@@ -6,7 +6,9 @@ version=$(cat info.json | jq -r .version)
 # Create git tag for this version
 git tag -f "$version"
 
+mkdir release
+
 # Prepare zip for Factorio native use and mod portal
 git archive --prefix "${name}_$version/" \
-  -o "${name}_$version.zip" \
+  -o "release/${name}_$version.zip" \
   HEAD
