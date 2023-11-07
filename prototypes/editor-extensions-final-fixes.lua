fd91fa518fa6985d6b551e563e264129d8ddacb1
Cameron Leger <contact@cameronleger.com>
2023-11-06 21:18:19 -0700

    Updated support for Editor Extensions

---
+++ prototypes/editor-extensions-final-fixes.lua
@@ -0,0 +1,3 @@
+data.raw["string-setting"]["ee-testing-lab"].hidden = true
+data.raw["string-setting"]["ee-testing-lab"].allowed_values = {"off"}
+data.raw["string-setting"]["ee-testing-lab"].default_value = "off"
